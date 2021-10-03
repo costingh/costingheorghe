64dd76d24d4d429496226177804cdd4077ea8b0f
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-10-03 13:57:53 +0300

    added 2 more projects

--- src/Components/HomeWorks.js
+++ src/Components/HomeWorks.js
@@ -28,7 +28,7 @@ function HomeWorks() {
                                         <div>
                                             <a
                                                 className="btn btn-work"
-                                                href="/"
+                                                href="https://costingh.github.io/chat-app"
                                                 target="_blank"
                                             >
                                                 View Project
@@ -63,7 +63,7 @@ function HomeWorks() {
                                         <div>
                                             <a
                                                 className="btn btn-work"
-                                                href="/"
+                                                href="https://collaborative-whiteboard.vercel.app/"
                                                 target="_blank"
                                             >
                                                 View Project
@@ -80,7 +80,85 @@ function HomeWorks() {
                                 </div>
                             </div>
                         </div>
-                        <div className="work-list calendar-works">
+                        <div className="work-list crisp-works">
+                            <div className="work-wrapper d-flex">
+                                <div className="work-description">
+                                    <div className="work-detail d-flex flex-column justify-content-between">
+                                        <div>
+                                            <h3>Social Media App</h3>
+                                            <p>
+                                                A social media application that
+                                                allows users to login/sign up,
+                                                and perform tasks such as
+                                                poosting pictures,
+                                                liking/unliking posts,
+                                                follow/unfollow another user,
+                                                and chat using sockets.
+                                            </p>
+                                            {/* <p>Spring Boot, MongoDB, React</p> */}
+                                        </div>
+                                        <div>
+                                            <a
+                                                className="btn btn-work"
+                                                href="https://github.com/costingh/social-media-app-MERN"
+                                                target="_blank"
+                                            >
+                                                View Project
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="work-image d-flex justify-content-center align-items-center">
+                                    <img
+                                        src="./assets/images/projects/social-media.svg"
+                                        style={{
+                                            maxWidth: '90%',
+                                        }}
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="work-list lifecycle-works">
+                            <div className="work-wrapper d-flex">
+                                <div className="work-description">
+                                    <div className="work-detail d-flex flex-column justify-content-between">
+                                        <div>
+                                            <h3>
+                                                Pathfinding Algorithm Visualiser
+                                            </h3>
+                                            <p>
+                                                App designed to illustrate how
+                                                popular pathfinding algorithms
+                                                (like Dijkstra, DFS, BFS, A*)
+                                                work. The algorithms are
+                                                implemented using JavaScript.
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <a
+                                                className="btn btn-work"
+                                                href="https://costingh.github.io/deploy-pathfinding-visualiser/"
+                                                target="_blank"
+                                            >
+                                                View Project
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="work-image d-flex justify-content-center align-items-center">
+                                    <img
+                                        src="./assets/images/projects/pathfinding.png"
+                                        style={{
+                                            maxWidth: '90%',
+                                            borderRadius: '10px',
+                                        }}
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        {/* <div className="work-list calendar-works">
                             <div className="work-wrapper d-flex">
                                 <div className="work-description">
                                     <div className="work-detail d-flex flex-column justify-content-between">
@@ -101,7 +179,7 @@ function HomeWorks() {
                                 </div>
                                 <div className="work-image"></div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
