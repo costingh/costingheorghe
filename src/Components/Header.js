f972c0a93897790e2f025db4ceef99e794da33c9
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-10-04 10:16:55 +0300

    changed scrollspy plugin

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ScrollSpy from 'react-scrollspy-navigation'
+/* import ScrollSpy from 'react-scrollspy-navigation' */
+
+import Scrollspy from 'react-scrollspy'
 
 function Header({ showContactModal }) {
     const toggleMenu = () => {
@@ -26,7 +28,9 @@ function Header({ showContactModal }) {
         if (st > lastScrollTop) {
             // downscroll code
             header.style.position = 'fixed'
-            header.style.backgroundColor = 'rgba(100,100,100,0.8)'
+            header.style.backgroundColor = 'rgba(100,100,100,1)'
+            /*  header.style.backgroundImage =
+                'linear-gradient(225deg, #1ad4bb, #b2de94)' */
             header.style.borderBottom = '1px solid #ef4b6c'
         } else {
             // upscroll code
@@ -50,74 +54,85 @@ function Header({ showContactModal }) {
                             </a>
                         </div> */}
                         <div className="header-menu">
-                            <ul className="nav nav-light">
-                                <ScrollSpy>
-                                    <li className="nav-item active">
-                                        <a
-                                            className="nav-link"
-                                            href="#header-section"
-                                            ref={React.createRef()}
-                                        >
-                                            Home
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            href="#about-section"
-                                            ref={React.createRef()}
-                                        >
-                                            About
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            href="#education-section"
-                                            ref={React.createRef()}
-                                        >
-                                            Education
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            href="#skills-section"
-                                            ref={React.createRef()}
-                                        >
-                                            Skills
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            href="#services-section"
-                                            ref={React.createRef()}
-                                        >
-                                            Services
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            href="#projects-section"
-                                            ref={React.createRef()}
-                                        >
-                                            Projects
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <span
-                                            className="nav-link"
-                                            data-toggle="modal"
-                                            data-target="#contactModal"
-                                            onClick={showContactModal}
-                                        >
-                                            Contact
-                                        </span>
-                                    </li>
-                                </ScrollSpy>
-                            </ul>
+                            <Scrollspy
+                                items={[
+                                    'home-section',
+                                    'about-section',
+                                    'education-section',
+                                    'skills-section',
+                                    'services-section',
+                                    'projects-section',
+                                ]}
+                                className="nav nav-light"
+                                currentClassName="active"
+                            >
+                                {/* <ul className="nav nav-light"> */}
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#home-section"
+                                        ref={React.createRef()}
+                                    >
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#about-section"
+                                        ref={React.createRef()}
+                                    >
+                                        About
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#education-section"
+                                        ref={React.createRef()}
+                                    >
+                                        Education
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#skills-section"
+                                        ref={React.createRef()}
+                                    >
+                                        Skills
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#services-section"
+                                        ref={React.createRef()}
+                                    >
+                                        Services
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        className="nav-link"
+                                        href="#projects-section"
+                                        ref={React.createRef()}
+                                    >
+                                        Projects
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <span
+                                        className="nav-link"
+                                        data-toggle="modal"
+                                        data-target="#contactModal"
+                                        onClick={showContactModal}
+                                    >
+                                        Contact
+                                    </span>
+                                </li>
+                                {/* </ul> */}
+                            </Scrollspy>
                             <div className="mobile-menu" onClick={toggleMenu}>
                                 <span></span>
                                 <span></span>
