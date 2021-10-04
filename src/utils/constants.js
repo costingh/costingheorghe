16c9bcc41b92c2409218746e3620d7f74c26a113
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-10-04 11:01:26 +0300

    added skills and tools images

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,91 +4,83 @@ export const timelineEvents = [
         to: 'May 2019',
         universityName: 'Matei Basarab National College',
         description: 'Mathematics & Computer Science',
-        classes: [
-            'Mathematics',
-            'Computer science',
-            'Physics',
-        ]
+        classes: ['Mathematics', 'Computer science', 'Physics'],
     },
     {
         from: 'September 2019',
         to: 'Present',
         universityName: 'Politehnica University of Bucharest',
         description: 'Mathematics & Computer Science',
-        classes: [
-            'Computer science',
-            'Mathematics',
-            'Physics',
-        ]
+        classes: ['Computer science', 'Mathematics', 'Physics'],
     },
-];
+]
 
 export const technologySkills = [
     {
-       img: './assets/images/skills/html5.png', 
-        delay: 600
+        img: './assets/images/skills/java.svg',
+        delay: 600,
     },
     {
-       img: './assets/images/skills/css3.png', 
-        delay: 600
+        img: './assets/images/skills/spring.svg',
+        delay: 600,
     },
     {
-       img: './assets/images/skills/bootstrap.png', 
-        delay: 500
+        img: './assets/images/skills/javascript.png',
+        delay: 500,
     },
     {
-       img: './assets/images/skills/javascript.png', 
-        delay: 500
+        img: './assets/images/skills/node.svg',
+        delay: 500,
     },
     {
-       img: './assets/images/skills/react.png', 
-        delay: 400
+        img: './assets/images/skills/react.svg',
+        delay: 400,
     },
     {
-       img: './assets/images/skills/jquery.png', 
-        delay: 400
+        img: './assets/images/skills/nextjs.svg',
+        delay: 400,
     },
     {
-       img: './assets/images/skills/sass.png', 
-        delay: 300
+        img: './assets/images/skills/mongo.svg',
+        delay: 300,
     },
     {
-       img: './assets/images/skills/bootstrap.png',     
-        delay: 300
+        img: './assets/images/skills/mysql.svg',
+        delay: 300,
     },
-];
+]
 
 export const toolsSkills = [
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 600
+        img: './assets/images/skills/vs-code.png',
+        delay: 600,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 600
+        img: './assets/images/skills/intellij.svg',
+        delay: 600,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 500
+        img: './assets/images/skills/git.svg',
+        delay: 500,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 500
+        img: './assets/images/skills/github.svg',
+        delay: 500,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 400
+        img: './assets/images/skills/docker.svg',
+        delay: 400,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 400
+        img: './assets/images/skills/figma.svg',
+        delay: 400,
     },
     {
-        img: './assets/images/skills/vs-code.png', 
-        delay: 300
+        img: './assets/images/skills/postman.svg',
+        delay: 300,
     },
     {
-        img: './assets/images/skills/vs-code.png',     
-        delay: 300
+        img: './assets/images/skills/firebase.png',
+        delay: 300,
     },
 ]
