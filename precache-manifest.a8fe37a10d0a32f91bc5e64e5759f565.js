5da0f4826f4a550c66c264a2a8d1f44dd0fe0b42
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-10-05 10:13:27 +0300

    Updates

---
+++ precache-manifest.a8fe37a10d0a32f91bc5e64e5759f565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02f84d18099f937c5fa5",
+    "url": "/costingheorghe/static/css/main.5f9f37e5.chunk.css"
+  },
+  {
+    "revision": "02f84d18099f937c5fa5",
+    "url": "/costingheorghe/static/js/main.d0fba55d.chunk.js"
+  },
+  {
+    "revision": "30ffb9f565de62922da1",
+    "url": "/costingheorghe/static/js/runtime~main.2cdc255c.js"
+  },
+  {
+    "revision": "7ea0de66d7e3f044908c",
+    "url": "/costingheorghe/static/js/2.b9eebdea.chunk.js"
+  },
+  {
+    "revision": "f9278dda0014302ab613",
+    "url": "/costingheorghe/static/js/3.bbb4e748.chunk.js"
+  },
+  {
+    "revision": "0aea8c029e91d3d980e1b10b0606f237",
+    "url": "/costingheorghe/index.html"
+  }
+];
